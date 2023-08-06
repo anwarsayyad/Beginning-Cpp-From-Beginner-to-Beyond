bf22b077dd173b5f3959454dba1a32caa11558f9
Aadarsh131 <aadarshkumar131@gmail.com>
2023-08-06 23:26:04 +0530

    modified code

--- 13. OOPS - Classes and Objects/Static Class Members/Player.cpp
+++ 13. OOPS - Classes and Objects/Static Class Members/Player.cpp
@@ -1,24 +1,33 @@
 #include "Player.h"
+#include <iostream>
+#include <iterator>
 
 // static attribute
-int Player::num_players{0}; // best place is to initialize static variables is here, in .cpp file (it should only be done once)
-// No need to specify static again here, as it is already declared in player.h file
-// best place is to assign it here, in .cpp file (it should only be done once)
+int Player::num_players{0}; // best place is to initialize static variables is
+                            // here, in .cpp file (it should only be done once)
+// No need to specify static again here, as it is already declared in player.h
+// file best place is to assign it here, in .cpp file (it should only be done
+// once)
 Player::Player(std::string name_val, int health_val, int xp_val)
-    : name{name_val}, health{health_val}, xp{xp_val}
-{
-    ++num_players;
+    : name{name_val}, health{health_val}, xp{xp_val} {
+  ++num_players;
 }
-Player::Player(const Player &source) : Player{source.name, source.health, source.xp}//as we are delegating the initializationi list, the main constructor will be called and static variable "num_players" will be incremented, everytime the object is copied 
-{
-}
-Player::~Player()
-{
-    --num_players;
+Player::Player(const Player &source)
+    : Player{source.name, source.health, source.xp}
+// as we are delegating the initializationi list, the main constructor will be
+// called and static variable "num_players" will be incremented, everytime the
+// object is copied
+{}
+Player::~Player() {
+  --num_players;
+  std::cout << "destructor called for: " << Player::name << std::endl;
 }
 
 // static method
-int Player::get_num_players() // as in our class declaration in player.h file, we have declared this method as static, hence we dont need to specify static again here in implementation part
+int Player::get_num_players() // as in our class declaration in player.h file,
+                              // we have declared this method as static, hence
+                              // we dont need to specify static again here in
+                              // implementation part
 {
-    return num_players;
+  return num_players;
 }
--- 13. OOPS - Classes and Objects/Static Class Members/main.cpp
+++ 13. OOPS - Classes and Objects/Static Class Members/main.cpp
@@ -1,29 +1,28 @@
-#include <iostream>
 #include "Player.h"
+#include <iostream>
 
 using namespace std;
 
-void displayActivePlayer()
-{
-    cout << "Active Players(total Player objects live): " << Player::get_num_players() << endl;
+void displayActivePlayer() {
+  cout << "Active Players(total Player objects live): "
+       << Player::get_num_players() << endl;
 }
 
-int main()
-{
-    displayActivePlayer();
-    Player p1;
-    Player p2;
-    displayActivePlayer();
+int main() {
+  displayActivePlayer();
+  Player p1{"p1"};
+  Player p2{"p2"};
+  displayActivePlayer();
 
-    {
-        Player p3;
-        displayActivePlayer();
-    }
+  {
+    Player p3;
     displayActivePlayer();
+  }
+  displayActivePlayer();
 
-    Player *p4 = new Player("Aadarsh", 100, 30);
-    displayActivePlayer();
-    delete p4; // automatic call of destructor with deallocation
-    displayActivePlayer();
-    return 0;
+  Player *p4 = new Player("Aadarsh", 100, 30);
+  displayActivePlayer();
+  delete p4; // automatic call of destructor with deallocation
+  displayActivePlayer();
+  return 0;
 }
